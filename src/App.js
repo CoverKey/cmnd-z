ca1cb5af9c1d70e89010a8c64fcfe84655cad1dd
CoverKey <cskinner@coverkey.com>
2020-06-09 14:27:24 -0700

    updating app with a shop page, collection preview component and collection

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-const HomePage = props => {
-  return(
-    <div>
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <h1>Home Page</h1>
-    </div>
-  );
-};
-
-const TopicsList = props => {
-  return(
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  return (
-    <div>
-      <h1> TOPIC DETAIL PAGE: {props.match.params.topicId} </h1>
-    </div>
-  );
-};
+import HomePage from './pages/homepage/homepage.componet';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
     return (
       <div>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-        <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-        <Route exact path='/blog/topics' component={TopicsList} />
-        <Route path='/blog/topics/:topicId' component={TopicDetail} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+        </Switch>
       </div>
     );
 }
