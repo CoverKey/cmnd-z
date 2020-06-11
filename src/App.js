35d933f4dffff6f5763c4e5815b90c51b7c74dde
CoverKey <cskinner@coverkey.com>
2020-06-11 14:02:25 -0700

    Firestore and Firebase, along with sign up and sign in completed.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import {
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.componet';
+import HomePage from './pages/homepage/homepage.componet.jsx';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument
+} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -24,12 +27,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({
+        currentUser: userAuth
+      });
     });
   }
 
@@ -40,11 +56,11 @@ class App extends React.Component {
   render() {
     return ( 
       <div>
-      <Header currentUser = {this.state.currentUser} /> 
+        <Header currentUser = {this.state.currentUser}/> 
         <Switch>
           <Route exact path = '/' component = {HomePage}/> 
-          <Route path = '/shop' component = {ShopPage} /> 
-          <Route path = '/signin' component = {SignInAndSignUpPage} /> 
+          <Route path = '/shop' component = {ShopPage}/> 
+          <Route path = '/signin' component = {SignInAndSignUpPage}/> 
         </Switch> 
       </div>
     );
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -32,8 +32,8 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="sign-in">
-        <h2>I already have an account.</h2>
-        <span>Sign in with your email and password.</span>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
@@ -55,7 +55,7 @@ class SignIn extends React.Component {
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-            Sign in with Google
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
